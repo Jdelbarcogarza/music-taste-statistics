4a14011c2817fe37d2e71a3cba44ddf12ea20b89
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-03-23 12:28:31 -0600

    top artists are now fetched

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -10,6 +10,8 @@ const Home: NextPage = () => {
   const [userSpotifyUserObject, setSpotifyUserObject] = useState<any>();
   const [accessToken, setAccessToken] = useState<any>("");
   const [topTracks, setTopTracks] = useState<any>([]);
+  const [topArtists, setTopArtists] = useState<any>([]);
+  const [timeFrame, setTimeFrame] = useState<string>("short_term");
   const router = useRouter();
 
   useEffect(() => {
@@ -25,46 +27,72 @@ const Home: NextPage = () => {
   }, []);
 
   useEffect(() => {
-    async function getAccessToken() {
+    async function getSpotifyData() {
+      // consegir access token
       const { data, error } = await supabase.auth.getSession();
-      setAccessToken(data.session?.provider_token);
+      setAccessToken(() => data.session?.provider_token);
+
+      // conseguir top tracks
+      const topTracks = await axios.get(
+        "https://api.spotify.com/v1/me/top/tracks",
+        {
+          headers: {
+            Authorization: `Bearer ${data.session?.provider_token}`,
+          },
+          params: {
+            limit: 6,
+            time_range: timeFrame
+          },
+        }
+      );
+
+      setTopTracks(() => topTracks.data.items);
+      console.log(topTracks.data.items);
+
+      // conseguir top artists
+      const topArtists = await axios.get(
+        "https://api.spotify.com/v1/me/top/artists",
+        {
+          headers: {
+            Authorization: `Bearer ${data.session?.provider_token}`,
+          },
+          params: {
+            limit: 6,
+          },
+        }
+      );
+
+      setTopArtists(() => topArtists.data.items);
+      console.log(topArtists.data.items);
     }
 
-    
-
-    getAccessToken();
-    // getTopTracks();
-
-  }, [userSpotifyUserObject]);
+    getSpotifyData();
+  }, [userSpotifyUserObject, timeFrame]);
   // npx supabase gen types typescript --project-id xhoehsaspitgrwvrrlaj --schema public > types/supabase.ts
-
-  
-  async function getTopTracks() {
-    const { data, error } = await supabase.auth.getSession();
-
-    const req = await axios.get("https://api.spotify.com/v1/me/top/tracks", {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-
-    console.log(req.data.items);
-    setTopTracks(req.data.items);
-
-  }
 
   return (
     <div>
+      <p>songs</p>
+
+      {/* <button onClick={getTopTracks} className="bg-green-400">
+          call API
+        </button> */}
+
+      <div>
+        {topTracks.map((object: any) => (
+          <p key={object.id}>{object.name}</p>
+        ))}
+      </div>
+
       <p>artists</p>
-
-      <button onClick={getTopTracks} className="bg-green-400">
-        call API
-      </button>
-
-      <div>{topTracks.map((object: any) => (
-        <p key={object.id}>{object.name}</p>
-      ))}</div>
+      <div>
+        {topArtists.map((object: any) => (
+          <p key={object.id}>{object.name}</p>
+        ))}
+      </div>
+      <button onClick={() => setTimeFrame("long_term")}>change</button>
     </div>
+
   );
 };
 
