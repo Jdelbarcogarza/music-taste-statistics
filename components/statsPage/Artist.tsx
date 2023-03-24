e5def01ee8c58ef878392cd90e73a67fa984d9d8
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-03-23 20:00:35 -0600

    se agregaron componentes de artistas y tracks

---
+++ components/statsPage/Artist.tsx
@@ -0,0 +1,17 @@
+
+import Image from "next/image";
+
+const Artist = ({ artist, className }: { artist: any, className: string; }) => {
+
+  return (
+    <div className={`flex flex-col items-center space-y-2 ${className}`} >
+      <Image src={artist.images[1].url} alt={`${artist.name}`} width={144} height={144} style={{
+        borderRadius: "100%"
+      }} />
+      <p className="text-center">{artist.name}</p>
+    </div>
+  );
+
+};
+
+export default Artist;
