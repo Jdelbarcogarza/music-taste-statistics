fe5b54d9bc87727b88ed13a5a6c609302c971bbd
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-03-23 20:00:59 -0600

    se agregó layout básico de stats page

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,26 +1,67 @@
 import { GetServerSideProps, NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { supabase } from "@/lib/supabaseInstance";
 import { useRouter } from "next/router";
 import axios from "axios";
+import { register } from "swiper/element/bundle";
+import Script from "next/script";
+
+import Track from "@/components/statsPage/Track";
+import Artist from "@/components/statsPage/Artist";
 
 const Home: NextPage = () => {
+  // swiper js
+  register();
+  const swiperElRefSongs = useRef(null);
+  const swiperElRefArtists = useRef(null);
+
   // validar si está validado el usuario, si no redirigir a login
 
   const [userSpotifyUserObject, setSpotifyUserObject] = useState<any>();
   const [accessToken, setAccessToken] = useState<any>("");
+
+  // tracks lists
   const [topTracks, setTopTracks] = useState<any>([]);
+
+  // artists lists
   const [topArtists, setTopArtists] = useState<any>([]);
-  const [timeFrame, setTimeFrame] = useState<string>("short_term");
+
+  const [artistsTimeFrame, setArtistTimeFrame] =
+    useState<string>("medium_term");
+  const [tracksTimeFrame, setTracksTimeFrame] = useState<string>("medium_term");
+
   const router = useRouter();
 
   useEffect(() => {
+    // listen for Swiper events using addEventListener
+    swiperElRefSongs.current.addEventListener("progress", (e) => {
+      const [swiper, progress] = e.detail;
+      // console.log(progress);
+    });
+
+    swiperElRefSongs.current.addEventListener("slidechange", (e) => {
+      // console.log("slide changed");
+    });
+
+    // listen for Swiper events using addEventListener
+    swiperElRefArtists.current.addEventListener("progress", (e) => {
+      const [swiper, progress] = e.detail;
+      // console.log(progress);
+    });
+
+    swiperElRefArtists.current.addEventListener("slidechange", (e) => {
+      // console.log("slide changed");
+    });
+
     async function getUserSession() {
       const {
         data: { user },
       } = await supabase.auth.getUser();
       setSpotifyUserObject(() => user);
       console.log("user", user);
+
+      const { data, error } = await supabase.auth.getSession();
+      setAccessToken(() => data.session?.provider_token);
     }
 
     getUserSession();
@@ -41,7 +82,7 @@ const Home: NextPage = () => {
           },
           params: {
             limit: 6,
-            time_range: timeFrame
+            time_range: tracksTimeFrame,
           },
         }
       );
@@ -58,6 +99,7 @@ const Home: NextPage = () => {
           },
           params: {
             limit: 6,
+            time_range: artistsTimeFrame,
           },
         }
       );
@@ -67,32 +109,48 @@ const Home: NextPage = () => {
     }
 
     getSpotifyData();
-  }, [userSpotifyUserObject, timeFrame]);
-  // npx supabase gen types typescript --project-id xhoehsaspitgrwvrrlaj --schema public > types/supabase.ts
+  }, [userSpotifyUserObject, artistsTimeFrame, tracksTimeFrame, accessToken]);
 
   return (
-    <div>
-      <p>songs</p>
+    <>
+      <Script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js" />
+      <div className="h-screen bg-black">
+        <h1 className="bg-red-200 text-3xl">Your Music Stats</h1>
+        <div className="flex h-5/6 flex-1 flex-col justify-around">
+          <div className="bg-teal-300">
+            <div className="mb-3 flex items-end justify-between px-3">
+              <p className="text-2xl">Artists</p>
+              <button className="pr-3 font-bold">See More</button>
+            </div>
+            <swiper-container ref={swiperElRefSongs} slides-per-view="3">
+              {topArtists.map((object: any) => (
+                <swiper-slide key={object.id} className={""}>
+                  <Artist artist={object} className={"mx-2"} />
+                </swiper-slide>
+              ))}
+            </swiper-container>
+          </div>
 
-      {/* <button onClick={getTopTracks} className="bg-green-400">
-          call API
-        </button> */}
-
-      <div>
-        {topTracks.map((object: any) => (
-          <p key={object.id}>{object.name}</p>
-        ))}
+          <div className="bg-orange-400">
+            <div className="mb-3 flex items-end justify-between px-3">
+              <p className="text-2xl">Songs</p>
+              <button className="pr-3 font-bold">See More</button>
+            </div>
+            <swiper-container ref={swiperElRefArtists} slides-per-view="3">
+              {topTracks.map((object: any) => (
+                <swiper-slide key={object.id} className={""}>
+                  <Track track={object} className={"mx-2"} />
+                </swiper-slide>
+              ))}
+            </swiper-container>
+          </div>
+        <div className="flex flex-col justify-center text-white px-4 space-y-3">
+          <p className="text-xl">Get to know in detail the music that you listen to.</p>
+          <button className="w-fit place-self-center border-2 rounded-lg py-4 px-3 border">Know what you hear</button>
+        </div>
+        </div>
       </div>
-
-      <p>artists</p>
-      <div>
-        {topArtists.map((object: any) => (
-          <p key={object.id}>{object.name}</p>
-        ))}
-      </div>
-      <button onClick={() => setTimeFrame("long_term")}>change</button>
-    </div>
-
+    </>
   );
 };
 
